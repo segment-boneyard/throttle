f377284310146a76179b61b9e70e4feb030c78ab
TJ Holowaychuk <tj@vision-media.ca>
2015-02-19 13:15:48 -0800

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,28 @@
+
+# throttle(1)
+
+ Tool for throttling JSON streams.
+
+## Installation
+
+```
+$ go get github.com/segmentio/throttle
+```
+
+## Usage
+
+```
+  Usage:
+    throttle <ops>
+    throttle <ops> <duration>
+    throttle -h | --help
+    throttle --version
+
+  Options:
+    -h, --help        output help information
+    -v, --version     output version
+```
+
+# License
+
+ MIT
---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import "github.com/tj/docopt"
+import "encoding/json"
+import "strconv"
+import "time"
+import "log"
+import "os"
+import "io"
+
+var Version = "0.0.1"
+
+const Usage = `
+  Usage:
+    throttle <ops>
+    throttle <ops> <duration>
+    throttle -h | --help
+    throttle --version
+
+  Options:
+    -h, --help        output help information
+    -v, --version     output version
+
+`
+
+func main() {
+	args, err := docopt.Parse(Usage, nil, true, Version, false)
+	if err != nil {
+		log.Fatalf("error parsing arguments: %s", err)
+	}
+
+	dur := time.Second
+	if s, ok := args["<duration>"].(string); ok {
+		dur, err = time.ParseDuration(s)
+		if err != nil {
+			log.Fatalf("error parsing duration: %s", err)
+		}
+	}
+
+	ops, err := strconv.Atoi(args["<ops>"].(string))
+	if err != nil {
+		log.Fatalf("error parsing ops: %s", err)
+	}
+
+	dec := json.NewDecoder(os.Stdin)
+	enc := json.NewEncoder(os.Stdout)
+	throttle := time.Tick(dur / time.Duration(ops))
+
+	for {
+		<-throttle
+
+		var v interface{}
+		err := dec.Decode(&v)
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatalf("error decoding: %s", err)
+		}
+
+		err = enc.Encode(v)
+
+		if err != nil {
+			log.Fatalf("error encoding: %s", err)
+		}
+	}
+}
